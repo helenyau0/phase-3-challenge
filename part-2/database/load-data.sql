c90f2c9e68ddf638f93ee149b33b7fedf4a23513
Helen Yau <helenyau13@gmail.com>
2017-07-31 17:05:44 -0700

    create database, schema, seed grocery_items table with cvs file, and write all scripts to create, drop, schema, and seed database

---
+++ part-2/database/load-data.sql
@@ -0,0 +1,2 @@
+COPY grocery_items(product_name,price,section)
+FROM '/Users/helen/Desktop/Phase 2/phase-3-challenge/part-2/database/grocery_items.cvs' DELIMITER ',' CSV HEADER;
---
+++ part-2/database/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS shoppers;
+
+CREATE TABLE shoppers (
+  id SERIAL PRIMARY KEY,
+  shopper VARCHAR(250),
+  number_of_orders INTEGER
+);
+
+DROP TABLE IF EXISTS grocery_items;
+
+CREATE TABLE grocery_items (
+  id SERIAL PRIMARY KEY,
+  product_name VARCHAR(250),
+  price DECIMAL,
+  section VARCHAR(250),
+  item_id INTEGER,
+  FOREIGN KEY(item_id) REFERENCES shoppers(id)
+);
+
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  total_cost DECIMAL,
+  total_id INTEGER,
+  FOREIGN KEY(total_id) REFERENCES shoppers(id)
+
+);
